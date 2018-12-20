3594be704ed07c337d65ab47e7a2a4afb685df01
Rahul Chauhan <rahul.chauhan@anz.com>
2018-12-20 15:53:49 +1100

    Moves logic from router to service

--- services/asciiService.js
+++ services/asciiService.js
@@ -1,5 +1,7 @@
 const logger = require('../utils/logger');
 const jpegJs = require('jpeg-js');
+const _ = require('lodash');
+const ndarray = require('ndarray')
 const ASCII_MAP = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. "
 
 const printImg = function (arr, width, height) {
@@ -28,14 +30,62 @@ const getAvgArray = function (raw) {
     return oneD
 }
 
+
+const getAvgArrayNew = function (raw, factor, width) {
+    let pixelArray = []
+    for (let i = 0; i < raw.length - 3; i += 4) {
+        pixelArray.push({
+            r: raw[i],
+            g: raw[i + 1],
+            b: raw[i + 2],
+            a: raw[i + 3]
+        });
+    }
+    logger.info("Total pixels: " + pixelArray.length)
+
+    let oneD = []
+    let sumOfArea = 0;
+    for (let i = 0; i < pixelArray.length ; i = i + ((width/factor)*factor*factor)) {
+        for (let j = i; j < i+width ; j+= factor) {
+            //if( i < 900){console.log(j)}
+            let area=[], temp =[]
+            for (let k = j; k < j+factor ; k++) {
+               for( let l=0, idx=k; l< factor; l++,idx= idx + width){
+                    if( i < 900){console.log(idx)}
+                    let pixel = pixelArray[idx];
+                    area.push(normalizePixel(pixel))
+                   // temp.push(idx)
+               }
+              // console.log("****")
+            }
+            
+            //if( j < 100){console.log(idx)}
+            oneD.push(_.mean(area));
+        }
+    }
+    //logger.error(oneD)
+    return oneD
+}
+
+const normalizePixel = function(pixel){
+    val = _.mean([pixel.r,pixel.g,pixel.b])
+   // logger.error(val)
+    return val
+}
+
 const toIndex = val => parseInt(val * 70 / 255)
 
-const render = function(data){
+const render = function (data) {
     //prepare pixel array
     let img = jpegJs.decode(data, true)
     //convert to grayscale
-    let oneD = getAvgArray(img.data)
-    logger.info('Image details: ' + { width: img.width, height: img.height})
+    let factor = 1;
+
+    let oneD = getAvgArrayNew(img.data, factor, img.width)
+    logger.info('Image details: ' + JSON.stringify({
+        width: img.width,
+        height: img.height
+    }))
     //render it as wall of text
     let ret = printImg(oneD, img.width, img.height)
     return ret
